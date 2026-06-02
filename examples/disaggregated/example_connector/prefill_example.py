5b411d57b37b151006d71757100e43daa8e32212
YAMAMOTO Takashi <yamamoto@midokura.com>
2026-06-02 05:09:09 +0000

    generate more tokens

--- examples/disaggregated/example_connector/prefill_example.py
+++ examples/disaggregated/example_connector/prefill_example.py
@@ -28,7 +28,7 @@ def read_prompts():
 def main():
     prompts = read_prompts()
 
-    sampling_params = SamplingParams(temperature=0, top_p=0.95, max_tokens=1)
+    sampling_params = SamplingParams(temperature=0, top_p=0.95, max_tokens=100)
 
     llm = LLM(
         #model="meta-llama/Llama-3.2-1B-Instruct",
